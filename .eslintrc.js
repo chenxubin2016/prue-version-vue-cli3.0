3711ee12d27d5eced570c489ffbae0cd64f224ac
陈旭斌 <xbchen@travelsky.com>
2020-08-31 09:19:48 +0800

    提交vue-cli3.0创建的vue项目纯净版本到github

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,17 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // allow async-await
+    'generator-star-spacing': 'off',
+    'allowEmptyReject': 0,
+    // allow debugger during development
+    'no-trailing-spaces': 'off',
+    'semi': ['error', 'always', {'omitLastInOneLineBlock': true}],
+    'one-var': ['error', {
+      'var': 'consecutive',
+      'let': 'consecutive',
+      'const': 'consecutive'
+    }]
   }
 };
